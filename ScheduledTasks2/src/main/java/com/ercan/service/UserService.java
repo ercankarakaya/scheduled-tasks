217068028c738137c85bf3050a6c094124eed380
Ercan Karakaya <ercankarakaya@ycp.com.tr>
2022-01-02 02:06:11 +0300

    code edited.

--- ScheduledTasks2/src/main/java/com/ercan/service/UserService.java
+++ ScheduledTasks2/src/main/java/com/ercan/service/UserService.java
@@ -3,20 +3,37 @@ package com.ercan.service;
 import com.ercan.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-
-import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private List<User> userList;
 
+    @Override
+    public void run(String... args) throws Exception {
+        userList=new ArrayList<>();
+        userList.add(new User(1, "John"));
+        userList.add(new User(2, "Ricardo"));
+        userList.add(new User(3, "Daniel"));
+        userList.add(new User(4, "Micheal"));
+        userList.add(new User(5, "Leonardo"));
+        userList.add(new User(6, "Scott"));
+        logger.info("All users are saved in the list.");
+    }
+
+
+    public List<User> getAllUser() {
+        return userList;
+    }
+
+    /*
     // initial block for user data
     {
         if (CollectionUtils.isEmpty(userList)){
@@ -30,10 +47,6 @@ public class UserService {
             logger.info("All users are saved in the list.");
         }
     }
-
-
-    public List<User> getAllUser() {
-        return userList;
-    }
+*/
 
 }
